262f05d9f3cab05bf7654997dec6a6a91104720e
tangbyron <tangbyron@gmail.com>
2025-11-24 15:32:09 +0000

    Deploying to gh-pages from @ tangbyron/tangbyron.github.io@e995afc57423bc37bdd48306b845f3462059942f 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,85 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "post-recursive-language-models-code-execution-60-accuracy-on-browsecomp-plus-no-embeddings",
+        
+          title: "Recursive Language Models + Code Execution: 60% accuracy on BrowseComp Plus (no embeddings)...",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/recursive-lm-code-execution/";
+          
+        },
+      },{id: "post-recursive-language-models-reduce-context-rot-and-2-5-accuracy-on-browsecomp-plus-at-2-6-latency",
+        
+          title: "Recursive Language Models reduce context rot and 2.5× accuracy on BrowseComp‑Plus (at 2.6×...",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/recursive-language-models/";
+          
+        },
+      },{id: "post-exploring-continuous-learning-reasoning-bank-recursive-language-models",
+        
+          title: "Exploring Continuous Learning: Reasoning Bank + Recursive Language Models",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/anti-patterns-as-guardrails/";
+          
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
